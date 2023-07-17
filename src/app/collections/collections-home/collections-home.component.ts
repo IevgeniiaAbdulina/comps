84704b821f0a4934426ae0f0bcbf93114abef92c
Ievgeniia Abdulina <ievgeniiaabdulina@gmail.com>
2023-07-17 12:25:53 +0200

    feat: Built a Reusable Table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
   styleUrls: ['./collections-home.component.css']
 })
-export class CollectionsHomeComponent {
+export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 30, job: 'Designer', employed: true },
+    { name: 'Jill', age: 28, job: 'Engineer', employed: false },
+    { name: 'Elyse', age: 32, job: 'Engineer', employed: true }
+  ];
+
+  headers = [
+    { key: 'employed', label: 'Has a Job?' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
